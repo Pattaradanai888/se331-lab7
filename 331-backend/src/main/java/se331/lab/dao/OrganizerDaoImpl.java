31f9e674884cbbd5fd6b182884441b0d88a0541f
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2024-09-03 13:34:17 +0700

    init with lab-07

---
+++ 331-backend/src/main/java/se331/lab/dao/OrganizerDaoImpl.java
@@ -0,0 +1,61 @@
+package se331.lab.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Organizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrganizerDaoImpl implements OrganizerDao {
+    List<Organizer> organizerList;
+
+    @PostConstruct
+    public void init() {
+        organizerList = new ArrayList<>();
+        organizerList.add(Organizer.builder()
+                .id(123L)
+                .name("Fern Pollin")
+                .address("Flora City")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(456L)
+                .name("Carey Wales")
+                .address("Playa Del Carmen")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(789L)
+                .name("Dawg Dahd")
+                .address("Woof Town")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(1001L)
+                .name("Kahn Opiner")
+                .address("Tin City")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(1002L)
+                .name("Brody Kill")
+                .address("Highway 50")
+                .build());
+    }
+
+    @Override
+    public Integer getOrganizerSize() {
+        return organizerList.size();
+    }
+
+    @Override
+    public List<Organizer> getOrganizers(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? organizerList.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page -1) * pageSize;
+        return organizerList.subList(firstIndex, firstIndex + pageSize);
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id) {
+        return organizerList.stream().filter(event -> event.getId().equals(id)).findFirst().orElse(null);
+    }
+}
---
+++ 331-backend/src/main/java/se331/lab/rest/controller/OrganizeController.java
@@ -0,0 +1,51 @@
+package se331.lab.rest.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import se331.lab.entity.Organizer;
+import se331.lab.service.OrganizerService;
+
+import java.util.List;
+
+@Controller
+public class OrganizeController {
+
+    final OrganizerService organizerService;
+
+    public OrganizeController(OrganizerService organizerService) {
+        this.organizerService = organizerService;
+    }
+
+    @GetMapping("organizer")
+    public ResponseEntity<?> getOrganizeLists(@RequestParam(value = "_limit",
+            required = false) Integer perPage
+            , @RequestParam(value = "_page", required = false)Integer page) {
+        List<Organizer> output = null;
+        Integer organizeSize = organizerService.getOrganizerSize();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("x-total-count", String.valueOf(organizeSize));
+        try {
+            output = organizerService.getOrganizers(perPage, page);
+        } catch (IndexOutOfBoundsException e) {
+            return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("organizer/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+        Organizer output = organizerService.getOrganizer(id);
+
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
+        }
+    }
+}
